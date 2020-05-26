1888c1250aa5d870681338cf60b9621fca859a57
Mohammed Rifas S <rifas2475@outlook.com>
2020-05-26 19:45:55 +0530

    IDE Change

--- spring-demo-one/src/spring/demo/one/HelloSpringApp.java
+++ spring-demo-one/src/spring/demo/one/HelloSpringApp.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spring.demo.one;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- *
- * @author Rifas
- */
 public class HelloSpringApp {
     public static void main(String[] args){
         // load the spring config files
--- spring-demo-one/src/spring/demo/one/SpringDemoOne.java
+++ spring-demo-one/src/spring/demo/one/SpringDemoOne.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spring.demo.one;
 
-/**
- *
- * @author Rifas
- */
 public class SpringDemoOne {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // create the object
         Coach theCoach = new TrackCoach();
