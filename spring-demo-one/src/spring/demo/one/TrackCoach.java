f2281c3974a73870eb229696c99442df4563298d
Mohammed Rifas S <rifas2475@outlook.com>
2020-05-26 19:44:58 +0530

    IDE Change

--- spring-demo-one/src/spring/demo/one/TrackCoach.java
+++ spring-demo-one/src/spring/demo/one/TrackCoach.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spring.demo.one;
 
-/**
- *
- * @author Rifas
- */
 public class TrackCoach implements Coach {
     
     @Override
