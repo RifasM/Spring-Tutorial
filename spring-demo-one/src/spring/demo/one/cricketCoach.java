d0f6316db2e1f7b4305075f993a9abf46ac4964b
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-02 15:40:51 +0530

    Setter Injection

---
+++ spring-demo-one/src/spring/demo/one/cricketCoach.java
@@ -0,0 +1,27 @@
+package spring.demo.one;
+
+public class cricketCoach implements Coach{
+    private FortuneService fortuneService;
+
+    // No argument Constructor
+
+    public cricketCoach() {
+        System.out.println("Inside No argument constructor of Cricket Coach");
+    }
+
+    // Setter method to be called by Spring
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("Inside Setter method of Cricket Coach");
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 30 minutes.";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+}
