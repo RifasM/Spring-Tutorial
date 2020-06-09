8adf3e2aa21a80fe515d2fe0e8a8431d421c7faf
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-09 19:46:40 +0530

    Spring Annotations Default

--- spring-demo-annotations/src/spring/demo/annotations/TennisCoach.java
+++ spring-demo-annotations/src/spring/demo/annotations/TennisCoach.java
@@ -2,7 +2,8 @@ package spring.demo.annotations;
 
 import org.springframework.stereotype.Component;
 
-@Component("theTennisCoach")    // beanID which spring calls
+// @Component("theTennisCoach")    // beanID which spring calls (explicit)
+@Component          // default BeanID - tennisCoach
 public class TennisCoach implements Coach{
     @Override
     public String getDailyWorkout() {
