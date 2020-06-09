6e04aca10686a7f474d85d909a7b202978782bc7
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-09 19:42:51 +0530

    Spring Annotations Restructure

---
+++ spring-demo-annotations/src/spring/demo/annotations/AnnotationDemoApp.java
@@ -0,0 +1,19 @@
+package spring.demo.annotations;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+    public static void main(String[] args) {
+        // Read Spring Config File
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Get Bean from Spring Container
+        Coach theCoach = context.getBean("theTennisCoach", Coach.class);
+
+        // Call a method on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // Close the context
+        context.close();
+    }
+}
