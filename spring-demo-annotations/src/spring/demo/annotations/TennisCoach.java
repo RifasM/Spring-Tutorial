9e990009ac0790ef9d68b062004c0866604587e3
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-19 20:15:02 +0530

    Method Injection - Autowiring

--- spring-demo-annotations/src/spring/demo/annotations/TennisCoach.java
+++ spring-demo-annotations/src/spring/demo/annotations/TennisCoach.java
@@ -3,6 +3,8 @@ package spring.demo.annotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.sql.SQLOutput;
+
 // @Component("theTennisCoach")    // beanID which spring calls (explicit)
 @Component          // default BeanID - tennisCoach
 public class TennisCoach implements Coach{
@@ -21,10 +23,19 @@ public class TennisCoach implements Coach{
         System.out.println("Inside Tennis Coach Default Constructor.");
     }
 
+    /*
     // Setter Injection
     @Autowired
     public void setFortuneService(FortuneService theFortuneService) {
         System.out.println("Inside Setter injection setFortuneService()");
+        this.fortuneService = theFortuneService;
+    }
+     */
+
+    // Method Injection
+    @Autowired
+    public void methodInjection(FortuneService theFortuneService){
+        System.out.println("Inside methodInjection");
         this.fortuneService = theFortuneService;
     }
 
