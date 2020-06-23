0ad91aeb33ae864320bb1560790cdf0616376565
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-23 20:06:14 +0530

    Dependency Injection for Java Class

--- spring-demo-annotations/src/spring/demo/annotations/SportConfig.java
+++ spring-demo-annotations/src/spring/demo/annotations/SportConfig.java
@@ -1,10 +1,22 @@
 package spring.demo.annotations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("spring.demo.annotations")
+//@ComponentScan("spring.demo.annotations")
 public class SportConfig {
 
+    // Define bean for sad fortune Service
+    @Bean
+    public FortuneService sadFortuneService(){ // method name is the bean ID
+        return new SadFortuneService();
+    }
+
+    // Define bean for swim coach and inject dependency
+    @Bean
+    public Coach swimCoach(){
+        return new SwimCoach(sadFortuneService()); // Dependency injection of sad fortune service in swimCOach
+    }
 }
