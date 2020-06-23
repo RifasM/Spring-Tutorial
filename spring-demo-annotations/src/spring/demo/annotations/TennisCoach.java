5f3243f776a5fa7a4d634a5bdb4d150342ac495f
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-23 19:39:04 +0530

    Init destroy methods

--- spring-demo-annotations/src/spring/demo/annotations/TennisCoach.java
+++ spring-demo-annotations/src/spring/demo/annotations/TennisCoach.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-
-import java.sql.SQLOutput;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 // @Component("theTennisCoach")    // beanID which spring calls (explicit)
 @Component          // default BeanID - tennisCoach
-@Scope("prototype")
+//@Scope("prototype")   // prototype scope annotation
 public class TennisCoach implements Coach{
 
     /*
@@ -66,5 +66,17 @@ public class TennisCoach implements Coach{
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
+    }
+
+    // Define initialise method
+    @PostConstruct
+    public void initMethod(){
+        System.out.println("Inside tennisCoach Initialise method.");
+    }
+
+    // Define Destroy/Cleanup method
+    @PreDestroy
+    public void destroyMethod(){
+        System.out.println("Inside tennisCoach destroy method.");
     }
 }
